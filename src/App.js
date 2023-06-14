d05f8acfafe447f3899b89b27d0f978f5889ad89
Nataliia Kalynovska <112168059+TaleQ@users.noreply.github.com>
2023-06-14 21:39:08 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+"use strict";
+exports.__esModule = true;
+var React = require("react");
+require("./App.css");
+function App() {
+    return (React.createElement("div", { className: 'App' },
+        React.createElement("header", { className: 'App-header' },
+            React.createElement("p", null,
+                "Edit ",
+                React.createElement("code", null, "src/App.tsx"),
+                " and save to reload."),
+            React.createElement("a", { className: 'App-link', href: 'https://reactjs.org', target: '_blank', rel: 'noopener noreferrer' }, "Learn React"))));
+}
+exports["default"] = App;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+"use strict";
+exports.__esModule = true;
+var React = require("react");
+var ReactDOM = require("react-dom/client");
+require("./index.css");
+var App_1 = require("./App");
+var root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(React.createElement(React.StrictMode, null,
+    React.createElement(App_1["default"], null)));
